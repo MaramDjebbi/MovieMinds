d19d0d8df23601f69bb52e0dda3be2a5ac435a5c
MaramDjebbi <djebbi.maram@etudiant-isi.utm.tn>
2023-12-21 17:02:23 +0100

    fixed dropDownMenu

--- code.py
+++ code.py
@@ -44,7 +44,7 @@ def recommend_movies(user_profile, movies):
 def get_user_profile():
     user_profile = {
         'liked_movies': liked_movies_entry.get().split(','),
-        'preferred_genre': preferred_genre_entry.get(),
+        'preferred_genre': preferred_genre_var.get(),
         'favorite_actors': favorite_actors_entry.get().split(','),
         'min_rating': float(min_rating_entry.get())
     }
@@ -68,31 +68,41 @@ canvas.pack(fill="both", expand=True)
 canvas.create_image(0, 0, image=bg_photo, anchor="nw")
 
 # Labels, Entry Fields, Buttons, and other elements on the canvas...
-label1 = tk.Label(canvas, text="Your Liked Movies (Comma-separated):")
+
+label1 = tk.Label(canvas, text="Your Liked Movies (Comma-separated):", fg="#118ab2")
 label1.place(x=320 , y=10)
 liked_movies_entry = tk.Entry(canvas)
 liked_movies_entry.place(x=365 , y=40)
 
-label2 = tk.Label(canvas, text="Your Preferred Genre:")
+genre_options = [
+    "Crime", "Action", "Drama", "Sci-Fi", "War", "Thriller",
+    "Fantasy", "Biography", "Adventure", "Western", "Comedy",
+    "Musical", "Romance"
+]
+
+label2 = tk.Label(canvas, text="Your Preferred Genre:", fg="#118ab2")
 label2.place(x=372 , y=70)
-preferred_genre_entry = tk.Entry(canvas)
-preferred_genre_entry.place(x=365 , y=100)
+# Dropdown menu for preferred genre
+preferred_genre_var = tk.StringVar(root)
+preferred_genre_var.set(genre_options[0])  # Set the default value
+preferred_genre_dropdown = tk.OptionMenu(canvas, preferred_genre_var, *genre_options)
+preferred_genre_dropdown.place(x=400, y=100)
 
-label3 = tk.Label(canvas, text="Your Favorite Actors (Comma-separated):")
-label3.place(x=310 , y=130)
+label3 = tk.Label(canvas, text="Your Favorite Actors (Comma-separated):", fg="#118ab2")
+label3.place(x=310 , y=140)
 favorite_actors_entry = tk.Entry(canvas)
-favorite_actors_entry.place(x=365 , y=160)
+favorite_actors_entry.place(x=365 , y=170)
 
-label4 = tk.Label(canvas, text="Minimum Movie Rating:")
-label4.place(x=372 , y=190)
+label4 = tk.Label(canvas, text="Minimum Movie Rating:", fg="#118ab2")
+label4.place(x=372 , y=200)
 min_rating_entry = tk.Entry(canvas)
-min_rating_entry.place(x=365 , y=220)
+min_rating_entry.place(x=365 , y=230)
 
-generate_button = tk.Button(canvas, text="Generate Recommendations", command=get_user_profile)
-generate_button.place(x=340 , y=255)
+generate_button = tk.Button(canvas, text="Generate Recommendations", command=get_user_profile, fg="#003049")
+generate_button.place(x=340 , y=265)
 
-recommended_movies_label = tk.Label(canvas, text="")
-recommended_movies_label.place(x=400 , y=280)
+recommended_movies_label = tk.Label(canvas, text="",fg="#003049")
+recommended_movies_label.place(x=320 , y=290)
 
 root.mainloop()
 
